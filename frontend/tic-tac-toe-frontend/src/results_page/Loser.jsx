26f6130ebbd24cff9cb18498d4d36d0ec8c331dd
Kashmira <kashmirajha2001@gmail.com>
2023-06-27 19:40:19 +0530

    Modified Loser.jsx

--- frontend/tic-tac-toe-frontend/src/results_page/Loser.jsx
+++ frontend/tic-tac-toe-frontend/src/results_page/Loser.jsx
@@ -1,16 +1,30 @@
-import './styles/style.css';
+import "./style.css";
 
 function Loser() {
   return (
     <div className="app">
+      <canvas id="Canvas"></canvas>  
+      <div class="wrapper">
+        <span>&#128531;</span>
+        <span>&#128532;</span>
+        <span>&#128534;</span>
+        <span>&#128547;</span>
+        <span>&#128553;</span>
+        <span>&#128546;</span>
+        <span>&#128557;</span>
+        <span>&#128560;</span>
+        <span>&#128555;</span>
+        <span>&#128542;</span>
+        <span>&#128532;</span>
+        <span>&#128546;</span>
+        <span>&#128546;</span>
+        <span>&#128560;</span>
+        <span>&#128555;</span>
+      </div>
       <header className="app-header-lose">
         <img src="./sad-gif.gif" className="gif" alt="gif" />
-        <p className="result">
-          Better luck next time!
-        </p>
-        <p className="lose">
-          You lost
-        </p>
+        <p className="result">Better luck next time!</p>
+        <p className="lose">You lost</p>
       </header>
     </div>
   );
