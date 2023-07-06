bc1e07868256201ce590a78bcb59e67f828f77a7
Subhradeep <subhradeeppal30@gmail.com>
2023-07-06 18:49:29 +0530

    scroll done

--- frontend/tic-tac-toe-frontend/src/game_page/chat_system/ChatBody.jsx
+++ frontend/tic-tac-toe-frontend/src/game_page/chat_system/ChatBody.jsx
@@ -3,18 +3,22 @@ import { ChatBodyWrapper } from "../styles/chatbody.styled";
 
 
 const ChatBody = () => {
-  const messagesRef = useRef(null);
+  // const messagesRef = useRef(null);
 
   useEffect(() => {
-    if (messagesRef.current) {
-      messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
-    }
-  }, [messagesRef]);
+
+    var messagesRef = document.getElementById("scrollChat");
+    setTimeout(() => {
+      messagesRef.scrollTop = messagesRef.scrollHeight;
+    }, 1500)
+
+
+  }, []);
 
 
   return (
-   
-    <div className="messages" ref={messagesRef}> 
+
+    <div className="messages" id="scrollChat">
       <ChatBodyWrapper>
         <div className="chat-messages"  >
           <div className="message receiver">
@@ -38,39 +42,39 @@ const ChatBody = () => {
             </div>
           </div>
           <div className="message sender">
-             <div className="content">
-               <p>Hello</p>
-             </div>
-           </div>
+            <div className="content">
+              <p>Hello</p>
+            </div>
+          </div>
 
           <div className="message sender">
             <div className="content">
-               <p>Hello</p>
+              <p>Hello</p>
             </div>
-           </div>
-         
-           <div className="message sender">
-             <div className="content">
-               <p>Hello</p>
-             </div>
-           </div>
+          </div>
 
-           <div className="message sender">
-             <div className="content">
-               <p>Hi,How are you?I am fine. You?</p>
-             </div>
-           </div>
+          <div className="message sender">
+            <div className="content">
+              <p>Hello</p>
+            </div>
+          </div>
+
+          <div className="message sender">
+            <div className="content">
+              <p>Hi,How are you?I am fine. You?</p>
+            </div>
+          </div>
 
           <div className="message receiver">
-             <div className="content">
-               <p>Hi,How are you?I am fine. You?</p>
-             </div>
-           </div>
-        
+            <div className="content">
+              <p>Hi,How are you?I am fine. You?</p>
+            </div>
+          </div>
+
         </div>
       </ChatBodyWrapper>
     </div>
-   
+
   );
 };
 
