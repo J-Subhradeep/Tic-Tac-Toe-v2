221295622a2872672be096c9fdaf2ef8b3f1f31a
Subhradeep <subhradeeppal30@gmail.com>
2023-07-12 22:25:50 +0530

    user ws

--- frontend/tic-tac-toe-frontend/src/game_page/Game.jsx
+++ frontend/tic-tac-toe-frontend/src/game_page/Game.jsx
@@ -2,12 +2,56 @@
 import Board from './board/Board'
 import Players from './players/Players'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { GameWrapper } from './styles/game.styled'
 import ChatSystem from './chat_system/ChatSystem'
 
 const Game = () => {
     // let roomCode = localStorage.getItem(roomCode)
+    // let name = localStorage.getItem('name')
+    // const [leftClient, setLeftClient] = useState("")
+    // const [rightClient, setRightClient] = useState("")
+
+    `
+    const [socketUrl, setSocketUrl] = useState('wss://api.play-real-tictactoe.cloud/api/ws/seconduser/{roomCode}/Subhradeep/');
+    const [messageHistory, setMessageHistory] = useState([]);
+  
+    const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+  
+    useEffect(() => {
+      if (lastMessage !== null) {
+        setMessageHistory((prev) => prev.concat(lastMessage));
+      }
+
+
+
+      lastMessage ->
+      {"group_name": "94843e3be49c4dfb9a115f3f4abf72fb",
+      "first_client": "Subhradeep",
+      "second_client": "koushik"}
+
+
+      const data = JSON.parse(lastMessage)
+      {group_name: "94843e3be49c4dfb9a115f3f4abf72fb",
+      first_client: "Subhradeep",
+      second_client: "koushik"}
+
+      if localStorage.getItem("name")==data.first_client:
+        leftClient = data.first_client
+      else if: data.second_client == null:
+        rightClient = "waiting..."
+      else:
+        
+
+
+
+
+
+    }, [lastMessage, setMessageHistory]);
+  
+
+
+    `
     return (
         <>
             <GameWrapper>
