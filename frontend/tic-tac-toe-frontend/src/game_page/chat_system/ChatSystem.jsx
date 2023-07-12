b44ff290f1bd620845d64d12a30554eb5b3dbd1d
Subhradeep <subhradeeppal30@gmail.com>
2023-07-12 21:30:50 +0530

    chat system documentation

--- frontend/tic-tac-toe-frontend/src/game_page/chat_system/ChatSystem.jsx
+++ frontend/tic-tac-toe-frontend/src/game_page/chat_system/ChatSystem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ChatWrapper } from "../styles/chatbox.styled";
 import img1 from "../../assets/images/game_page/chat.jfif";
 // import img2 from "../../assets/images/game_page/close-icon-30.png";
@@ -23,7 +23,64 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
 }));
 
 const ChatSystem = () => {
+  // const [messages, setMessages] = useState([{from:"",to:"",msg:""},{from:"",
+  //   to:"",
+  //   msg:""},{from:"",
+  //   to:"",
+  //   msg:""}
+  // ]);
   
+  // const [socketUrl, setSocketUrl] = useState('wss://api.play-real-tictactoe.cloud/api/ws/chat/localstorage.getitem('roomCode')_chat/x/');
+  // const [messageHistory, setMessageHistory] = useState([]);
+
+  // const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+
+  // useEffect(() => { // whenever a message is received
+  //   if (lastMessage !== null) {
+  //     setMessageHistory((prev) => prev.concat(lastMessage));
+  //   }
+    `1 lastMessage -> {"from":"koushik", "to":"subhradeep", "msg":"hi"}
+    
+    var data = convert javascript object JSON.parse
+    {
+      from:"",to:"",msg:"
+    }
+
+    localStorage -> name, symbol, roomcode
+    from compare localStorage.getItem(name)
+
+    if matches :
+      I have sent it
+    else:
+      other user
+    `
+  // }, [lastMessage, setMessageHistory]);
+
+  // const handleClickChangeSocketUrl = useCallback(
+  //   () => setSocketUrl('wss://demos.kaazing.com/echo'),
+  //   []
+  // );
+
+  // const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
+
+  // const connectionStatus = {
+  //   [ReadyState.CONNECTING]: 'Connecting',
+  //   [ReadyState.OPEN]: 'Open',
+  //   [ReadyState.CLOSING]: 'Closing',
+  //   [ReadyState.CLOSED]: 'Closed',
+  //   [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  // }[readyState];
+
+  
+  // const send = (message)=>{
+  //   sendMessage(
+  //     JSON.stringify({
+  //       from:
+  //       to:
+  //       message:
+  //     })
+  //   )
+  // }
   return (
     <ChatWrapper>
       <div className="container">
