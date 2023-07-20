790f01264a59e3706d6a091e2e16440fb5027aa0
Kashmira <kashmirajha2001@gmail.com>
2023-07-20 12:16:42 +0530

    completed user component

--- frontend/tic-tac-toe-frontend/src/game_page/Game.jsx
+++ frontend/tic-tac-toe-frontend/src/game_page/Game.jsx
@@ -6,22 +6,28 @@ import { GameWrapper } from "./styles/game.styled";
 import ChatSystem from "./chat_system/ChatSystem";
 import { useEffect } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
+import { EightMp } from "@mui/icons-material";
+import Winner from "../results_page/Winner";
 
 const Game = () => {
   let roomCode = localStorage.getItem("roomCode");
   let username = localStorage.getItem("name");
   const [leftClient, setLeftClient] = useState("");
   const [rightClient, setRightClient] = useState("");
+  const [flag, setFlag] = useState(true);
 
   const [socketUrl, setSocketUrl] = useState(
     "wss://api.play-real-tictactoe.cloud/api/ws/seconduser/" +
-      roomCode +"/" + username + "/"
+      roomCode +
+      "/" +
+      username +
+      "/"
   );
   const [messageHistory, setMessageHistory] = useState([]);
   const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
 
   useEffect(() => {
-    if (lastMessage !== null){
+    if (lastMessage !== null) {
       setMessageHistory((prev) => prev.concat(lastMessage));
       const clientData = JSON.parse(lastMessage.data);
       if (localStorage.getItem("name") == clientData.first_client) {
@@ -30,55 +36,62 @@ const Game = () => {
       }
       if (clientData.second_client === null) {
         setRightClient("Waiting...");
-      } 
-      else if (localStorage.getItem("name") === clientData.second_client) {
+      } else if (localStorage.getItem("name") === clientData.second_client) {
         setLeftClient(clientData.second_client);
         setRightClient(clientData.first_client);
+      }
+      if (clientData.second_client === false) {
+        setRightClient("Disconnected");
+        setFlag(false);
       }
     }
   }, [lastMessage, setMessageHistory]);
 
   return (
     <>
-      <GameWrapper>
-        <ChatSystem />
-        <div className="wrapper">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <div className="banner">
-          <div className="room-code">
-            Room Code: <br /> {localStorage.getItem("roomCode")}
+      {flag ? (
+        <GameWrapper>
+          <ChatSystem />
+          <div className="wrapper">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
-          <div className="upper">
-            <div className="player">
-              <Players name={leftClient} />
+          <div className="banner">
+            <div className="room-code">
+              Room Code: <br /> {localStorage.getItem("roomCode")}
             </div>
-            <div className="board">
-              <Board />
+            <div className="upper">
+              <div className="player">
+                <Players name={leftClient} />
+              </div>
+              <div className="board">
+                <Board />
+              </div>
+              <div className="player">
+                <Players name={rightClient} />
+              </div>
             </div>
-            <div className="player">
-              <Players name={rightClient} />
+            <div className="bottom">
+              <div className="player2">
+                <Players name={leftClient} />
+              </div>
+              <div className="player2">
+                <Players name={rightClient} />
+              </div>
             </div>
           </div>
-          <div className="bottom">
-            <div className="player2">
-              <Players name={leftClient} />
-            </div>
-            <div className="player2">
-              <Players name={rightClient} />
-            </div>
-          </div>
-        </div>
-      </GameWrapper>
+        </GameWrapper>
+      ) : (
+        <Winner />
+      )}
     </>
   );
 };
