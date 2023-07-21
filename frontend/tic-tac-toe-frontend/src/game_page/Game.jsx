e0f88bccd64c9f60f6acc9985f153f76e59ca69e
Safiul Haque <mdsafiulhaque4@gmail.com>
2023-07-22 02:07:49 +0530

    22/7/2023

--- frontend/tic-tac-toe-frontend/src/game_page/Game.jsx
+++ frontend/tic-tac-toe-frontend/src/game_page/Game.jsx
@@ -1,4 +1,5 @@
 import Board from "./board/Board";
+import Button from '@mui/material/Button';
 import Players from "./players/Players";
 import React, { useState } from "react";
 import { GameWrapper } from "./styles/game.styled";
@@ -7,84 +8,88 @@ import { useEffect } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
 
 const Game = () => {
-  let roomCode = localStorage.getItem("roomCode");
-  let username = localStorage.getItem("name");
-  const [leftClient, setLeftClient] = useState("");
-  const [rightClient, setRightClient] = useState("");
+    let roomCode = localStorage.getItem("roomCode");
+    let username = localStorage.getItem("name");
+    const [leftClient, setLeftClient] = useState("");
+    const [rightClient, setRightClient] = useState("");
 
-  const [socketUrl, setSocketUrl] = useState(
-    "wss://api.play-real-tictactoe.cloud/api/ws/seconduser/" +
-      roomCode +"/" + username + "/"
-  );
-  const [messageHistory, setMessageHistory] = useState([]);
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
-    shouldReconnect: (closeEvent) => true,
-    reconnectAttempts: 10,
-    reconnectInterval: 2000,
-  });
-
-  useEffect(() => {
-    if (lastMessage !== null){
-      setMessageHistory((prev) => prev.concat(lastMessage));
-      const clientData = JSON.parse(lastMessage.data);
-      if (localStorage.getItem("name") == clientData.first_client) {
-        setLeftClient(clientData.first_client);
-        setRightClient(clientData.second_client);
-      }
-      if (clientData.second_client === null) {
-        setRightClient("Waiting...");
-      } 
-      else if (localStorage.getItem("name") === clientData.second_client) {
-        setLeftClient(clientData.second_client);
-        setRightClient(clientData.first_client);
-      }
+    const [socketUrl, setSocketUrl] = useState(
+        "wss://api.play-real-tictactoe.cloud/api/ws/seconduser/" +
+        roomCode + "/" + username + "/"
+    );
+    function copyText() {
+        const content = localStorage.getItem('roomCode');
+        navigator.clipboard.writeText(content);
     }
-  }, [lastMessage, setMessageHistory]);
+    const [messageHistory, setMessageHistory] = useState([]);
+    const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
+        shouldReconnect: (closeEvent) => true,
+        reconnectAttempts: 10,
+        reconnectInterval: 2000,
+    });
+
+    useEffect(() => {
+        if (lastMessage !== null) {
+            setMessageHistory((prev) => prev.concat(lastMessage));
+            const clientData = JSON.parse(lastMessage.data);
+            if (localStorage.getItem("name") == clientData.first_client) {
+                setLeftClient(clientData.first_client);
+                setRightClient(clientData.second_client);
+            }
+            if (clientData.second_client === null) {
+                setRightClient("Waiting...");
+            }
+            else if (localStorage.getItem("name") === clientData.second_client) {
+                setLeftClient(clientData.second_client);
+                setRightClient(clientData.first_client);
+            }
+        }
+    }, [lastMessage, setMessageHistory]);
 
 
-  return (
-    <>
-      <GameWrapper>
-        <ChatSystem />
-        <div className="wrapper">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <div className="banner">
-          <div className="room-code">
-            Room Code: <br /> {localStorage.getItem("roomCode")}
-          </div>
-          <div className="upper">
-            <div className="player">
-              <Players name={leftClient} />
-            </div>
-            <div className="board">
-              <Board />
-            </div>
-            <div className="player">
-              <Players name={rightClient} />
-            </div>
-          </div>
-          <div className="bottom">
-            <div className="player2">
-              <Players name={leftClient} />
-            </div>
-            <div className="player2">
-              <Players name={rightClient} />
-            </div>
-          </div>
-        </div>
-      </GameWrapper>
-    </>
-  );
+    return (
+        <>
+            <GameWrapper>
+                <ChatSystem />
+                <div className="wrapper">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className="banner">
+                    <div className='room-code'>
+                        <Button onClick={copyText} variant="outlined">Copy Room Code</Button>
+                    </div>
+                    <div className="upper">
+                        <div className="player">
+                            <Players name={leftClient} />
+                        </div>
+                        <div className="board">
+                            <Board />
+                        </div>
+                        <div className="player">
+                            <Players name={rightClient} />
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <div className="player2">
+                            <Players name={leftClient} />
+                        </div>
+                        <div className="player2">
+                            <Players name={rightClient} />
+                        </div>
+                    </div>
+                </div>
+            </GameWrapper>
+        </>
+    );
 };
 
 export default Game;
--- frontend/tic-tac-toe-frontend/src/game_page/board/Board.jsx
+++ frontend/tic-tac-toe-frontend/src/game_page/board/Board.jsx
@@ -1,11 +1,69 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useCallback, useEffect } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { BoxWrapper } from '../styles/Board.styled'
 import SmallBox from '../small-box/SmallBox'
-import { convertLength } from '@mui/material/styles/cssUtils'
 
 
 const Board = () => {
 
+
+  const [boardElements, setBoardElements] = useState(['.', '.', '.', '.', '.', '.', '.', '.','.'])
+  const [lastSymbol, setLastSymbol] = useState('.')
+  const [lastBox, setLastBox] = useState('')
+
+  const [socketUrl, setSocketUrl] = useState(`wss://api.play-real-tictactoe.cloud/api/ws/board/${localStorage.getItem('roomCode')}_board/`);
+  // const [socketUrl2, setSocketUrl2] = useState(`wss://api.play-real-tictactoe.cloud/api/ws/seconduser/${localStorage.getItem('roomCode')}/${localStorage.getItem('name')}/`);
+  // console.log(socketUrl2)
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+  // const { sendMessage2, lastMessage2, readyState2 } = useWebSocket(socketUrl2);
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage));
+      setBoardElements(JSON.parse(lastMessage.data).arr)
+      setLastSymbol(JSON.parse(lastMessage.data).lastSymbol)
+      setLastBox(JSON.parse(lastMessage.data).lastBox)
+    }
+    // getting last message
+  }, [lastMessage, setMessageHistory]);
+
+
+  const handleClickOnBoardElement = (e) => {
+    // identify the element/index
+    let index = e.currentTarget.className
+    // console.log(index)
+
+    if (boardElements[index] == 'x' || boardElements[index] == 'o') {
+      console.log('already clkd')
+    }
+    else if(lastSymbol == localStorage.getItem('symbol')){
+      console.log('next turn')
+    }
+    else {
+
+        const newState = boardElements
+        if(newState[index] == 'x' || newState[index] == 'o'){
+
+        } else{
+          newState[index] = localStorage.getItem('symbol')
+        }
+
+        
+        // setBoardElements(newState);
+        sendMessage(JSON.stringify({ arr: newState, lastSymbol: localStorage.getItem('symbol'), lastBox: index }))
+        // console.log(boardElements)
+
+    }
+    // console.log('clickedboard')
+  }
+
+  // console.log(boardElements)
+  // console.log(messageHistory)
+
+
+  // const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
 
   let a = [0, 4, 7]
 
@@ -22,58 +80,22 @@ const Board = () => {
   }
 
   // win(a)
-  // const [boardElements, setBoardElements] = useState(['.', '.', 'X', '0', '.', '.', '.', 'X'])
-  // const [socketUrl, setSocketUrl] = useState(
-  //   "wss://api.play-real-tictactoe.cloud/api/ws/board/" + room + "_board" + "/"
-  // );
-  // const [messageHistory, setMessageHistory] = useState([]);
-  // const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, { shouldReconnect: (CloseEvent) => true, reconnectAttempts: 10, reconnectInterval: 2000 });
-  // useEffect(() => {
-    // trigger when new message is received
 
-    // if (lastMessage !== null) {
-    //   setMessageHistory((prev) => prev.concat(lastMessage));
-    // }
-
-
-    // getting last message
-    // const data = Json.parse->lastMessage
-    // {arr:size(9),lastSymbol:'X'}
-    // setBoardElements
-
-
-  // }, [lastMessage, messageHistory])
-
-  // const handleClickOnBoardElement = (e) => {
-    // console.log(e.id); 
-    // identify the element/index
-
-    // localstorage symbol x/0
-
-    // const arr = boardElements;
-    // if(arr[index]==x/0)
-    // else{
-      // arr[index] = x/0
-    // }
-    // sendMessage(JSON.stringify({arr:arr, lastSymbol:localStorage.getItem(symbol)}))
-
-
-  // }
   return (
     <>
       <BoxWrapper>
         {/* <button onClick={()=>win(a)}>c</button> */}
         <div className='box'>
           <div className='game-box'>
-            <SmallBox id='0' />
-            <SmallBox id='1' />
-            <SmallBox id='2' />
-            <SmallBox id='3' />
-            <SmallBox id='4' />
-            <SmallBox id='5' />
-            <SmallBox id='6' />
-            <SmallBox id='7' />
-            <SmallBox id='8' />
+            <div className='0' onClick={handleClickOnBoardElement}><SmallBox id='0' arr={boardElements} /></div>
+            <div className='1' onClick={handleClickOnBoardElement}><SmallBox id='1' arr={boardElements} /></div>
+            <div className='2' onClick={handleClickOnBoardElement}><SmallBox id='2' arr={boardElements} /></div>
+            <div className='3' onClick={handleClickOnBoardElement}><SmallBox id='3' arr={boardElements} /></div>
+            <div className='4' onClick={handleClickOnBoardElement}><SmallBox id='4' arr={boardElements} /></div>
+            <div className='5' onClick={handleClickOnBoardElement}><SmallBox id='5' arr={boardElements} /></div>
+            <div className='6' onClick={handleClickOnBoardElement}><SmallBox id='6' arr={boardElements} /></div>
+            <div className='7' onClick={handleClickOnBoardElement}><SmallBox id='7' arr={boardElements} /></div>
+            <div className='8' onClick={handleClickOnBoardElement}><SmallBox id='8' arr={boardElements} /></div>
           </div>
         </div>
       </BoxWrapper>
