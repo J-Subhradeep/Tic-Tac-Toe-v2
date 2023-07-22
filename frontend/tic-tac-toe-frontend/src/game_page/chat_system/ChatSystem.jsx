b1765c36f68c6dd628e35649f2acdfb32c08ef30
Safiul Haque <mdsafiulhaque4@gmail.com>
2023-07-22 11:52:02 +0530

    chat added

---
+++ frontend/tic-tac-toe-frontend/src/game_page/chat_system/ChatSystem.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import { ChatWrapper } from "../styles/chatbox.styled";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import ChatHeader from "./ChatHeader";
+import ChatBody from "./ChatBody";
+import ChatSend from "./ChatSend";
+import PopIcon from "./PopIcon";
+
+const ChatSystem = () => {
+  const [socketUrl, setSocketUrl] = useState(
+    "wss://api.play-real-tictactoe.cloud/api/ws/chat/" +
+      localStorage.getItem("roomCode") +
+      "_chat/" +
+      localStorage.getItem("symbol") +
+      "/"
+  );
+
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  // Unseen chats
+  const [unseenChatsFromOthers, setUnseenChatsFromOthers] = useState(0);
+  const [unseenChatsFromSender, setUnseenChatsFromSender] = useState(0);
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
+    shouldReconnect: (closeEvent) => true,
+    reconnectAttempts: 10,
+    reconnectInterval: 2000,
+  });
+
+  useEffect(() => {
+    // Whenever a message is received
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => [...prev, JSON.parse(lastMessage.data)]);
+
+      // To check unseen Chats from others
+      const newMessage = JSON.parse(lastMessage.data);
+      if (newMessage.from === localStorage.getItem("symbol")) {
+     
+        setUnseenChatsFromSender((prevCount) => prevCount + 1);
+      } else {
+        setUnseenChatsFromOthers((prevCount) => prevCount + 1);
+      }
+    }
+  }, [lastMessage, setMessageHistory]);
+
+  // Function to reset unseenChats to zero
+  const markMessagesAsSeen = () => {
+    setUnseenChatsFromSender(0);
+    setUnseenChatsFromOthers(0);
+  };
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
+
+  useEffect(() => {
+    console.log("WebSocket Connection Status:", readyState);
+  }, [readyState]);
+
+  return (
+    <ChatWrapper>
+      <div className="container">
+        <PopIcon
+          unseenChats={localStorage.getItem("symbol") === "sender" ? unseenChatsFromSender : unseenChatsFromOthers}
+          onClick={markMessagesAsSeen}
+        />
+
+        <div className="chat-popup" id="myForm">
+          <ChatHeader />
+          <ChatBody messageHistory={messageHistory} />
+          <ChatSend sendMessage={sendMessage} />
+        </div>
+      </div>
+    </ChatWrapper>
+  );
+};
+
+export default ChatSystem;
---
+++ frontend/tic-tac-toe-frontend/src/game_page/chat_system/PopIcon.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { PopIconWrapper } from "../styles/popicon.styled";
+import img1 from "../../assets/images/game_page/chat.jfif";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+
+const PopIcon = ({ unseenChats, onClick}) => {
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      right: -100,
+      top: -90,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: "0 4px",
+    },
+  }));
+
+  // right: -100,
+  // top: -90,
+
+  const openForm = () => {
+    document.getElementById("myForm").style.display = "block";
+    onClick();
+  };
+  return (
+    <div
+      className="whole"
+      style={{ position: "fixed", bottom: "15px", right: "15px" }}
+    >
+      <PopIconWrapper>
+        <div className="icon">
+          {unseenChats > 0 && ( 
+            <StyledBadge
+              badgeContent={unseenChats}
+              color="secondary"
+              className="badge"
+            ></StyledBadge>
+          )}
+          <img
+            src={img1}
+            alt="logo"
+            className=" transition-3 chatButton"
+            onClick={openForm}
+          />
+
+          <div className="transition-3 hello">Let's Chat 🤙</div>
+        </div>
+        {/* </StyledBadge> */}
+      </PopIconWrapper>
+    </div>
+  );
+};
+
+export default PopIcon;
