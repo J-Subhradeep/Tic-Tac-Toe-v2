e367eafee2ce15a6a5f4c53bfc4258fb0c30b6f8
Pratyush <pratyushbanerjeeadv6@gmail.com>
2023-08-02 20:59:48 +0530

    Guide page complete

--- frontend/tic-tac-toe-frontend/src/game_guide/styles/guide.style.jsx
+++ frontend/tic-tac-toe-frontend/src/game_guide/styles/guide.style.jsx
@@ -2,49 +2,42 @@ import { createGlobalStyle, css } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 body{
-    background-color: #2196f3;  
+    background-color: #132F4C;  
 }
-.goBack{
-    width: 100vw;
-    position: absolute;
-    height: 100vh;
-    background-color: #2196f3;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 15px;
+.headA{
+    font-size: 30px;
+}
+.headB{
+    font-size: 25px;
 }
 .gameSs{
     ${'' /* background-color: #2196f3; */}
     ${'' /* height: 100vh; */}
+    margin-top: 30px;
+    margin-bottom: 10px;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap: 24px;
+    gap: 10px;
 }
 .gameSsIn{
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap: 5px;
+    gap: 10px;
 }
 ${(props) =>
     props.responsive &&
     css`
     @media (max-width: 1050px)
     {
-        .goBack{
-            height: max-content;
-            padding-top: 10px;
-            padding-bottom: 10px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            gap: 15px;
+        .headA{
+            font-size: 15px;
+        }
+        .headB{
+            font-size: 15px;
         }
        .gameSs{
         height: fit-content;
@@ -54,6 +47,13 @@ ${(props) =>
         align-items: center;
         gap: 5px;
        } 
+       .gameSsIn{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 5px;
+       }
     }
     `
     }
